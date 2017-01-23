6674f9e14893ee07a40f403f617f93c261370b24
niftykins <niftykins@gmail.com>
2017-01-23 17:10:10 +0800

    add the rest of the buttons

--- src/constants/dummy.js
+++ src/constants/dummy.js
@@ -272,24 +272,430 @@ export const buttons = [
 				CLASSES.MONK
 			]
 		}
+	},
+
+
+	{
+		id: newId('button'),
+
+		name: 'Cloth',
+		order: 2,
+
+		select: {
+			[ROLES.MELEE]: [],
+			[ROLES.RANGED]: [
+				CLASSES.PRIEST,
+				CLASSES.MAGE,
+				CLASSES.WARLOCK
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.PRIEST
+			],
+			[ROLES.TANKS]: []
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Leather',
+		order: 2,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DRUID,
+				CLASSES.MONK,
+				CLASSES.ROGUE,
+				CLASSES.DEMON_HUNTER
+			],
+			[ROLES.RANGED]: [
+				CLASSES.DRUID
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.DRUID,
+				CLASSES.MONK
+			],
+			[ROLES.TANKS]: [
+				CLASSES.DRUID,
+				CLASSES.MONK,
+				CLASSES.DEMON_HUNTER
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Mail',
+		order: 2,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.SHAMAN
+			],
+			[ROLES.RANGED]: [
+				CLASSES.HUNTER,
+				CLASSES.SHAMAN
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.SHAMAN
+			],
+			[ROLES.TANKS]: []
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Plate',
+		order: 2,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.PALADIN,
+				CLASSES.WARRIOR
+			],
+			[ROLES.RANGED]: [],
+			[ROLES.HEALERS]: [
+				CLASSES.PALADIN
+			],
+			[ROLES.TANKS]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.PALADIN,
+				CLASSES.WARRIOR
+			]
+		}
+	},
+
+
+	{
+		id: newId('button'),
+
+		name: 'Conq',
+		order: 3,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.PALADIN,
+				CLASSES.DEMON_HUNTER
+			],
+			[ROLES.RANGED]: [
+				CLASSES.PRIEST,
+				CLASSES.WARLOCK
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.PALADIN,
+				CLASSES.PRIEST
+			],
+			[ROLES.TANKS]: [
+				CLASSES.PALADIN,
+				CLASSES.DEMON_HUNTER
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Vanq',
+		order: 3,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.DRUID,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.DRUID,
+				CLASSES.MAGE
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.DRUID
+			],
+			[ROLES.TANKS]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.DRUID
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Prot',
+		order: 3,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.WARRIOR,
+				CLASSES.SHAMAN,
+				CLASSES.MONK
+			],
+			[ROLES.RANGED]: [
+				CLASSES.HUNTER,
+				CLASSES.SHAMAN
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.SHAMAN,
+				CLASSES.MONK
+			],
+			[ROLES.TANKS]: [
+				CLASSES.WARRIOR,
+				CLASSES.MONK
+			]
+		}
+	},
+
+
+	{
+		id: newId('button'),
+
+		name: 'Arcane',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [],
+			[ROLES.RANGED]: [
+				CLASSES.MAGE,
+				CLASSES.DRUID,
+				CLASSES.HUNTER
+			],
+			[ROLES.HEALERS]: [],
+			[ROLES.TANKS]: [
+				CLASSES.PALADIN,
+				CLASSES.DEMON_HUNTER
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Blood',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.WARRIOR,
+				CLASSES.DRUID,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.WARLOCK,
+				CLASSES.PRIEST,
+				CLASSES.HUNTER
+			],
+			[ROLES.HEALERS]: [],
+			[ROLES.TANKS]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.WARRIOR,
+				CLASSES.DRUID
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Fel',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEMON_HUNTER,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.WARLOCK
+			],
+			[ROLES.HEALERS]: [],
+			[ROLES.TANKS]: [
+				CLASSES.DEMON_HUNTER
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Fire',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.PALADIN,
+				CLASSES.WARRIOR,
+				CLASSES.SHAMAN
+			],
+			[ROLES.RANGED]: [
+				CLASSES.MAGE,
+				CLASSES.WARLOCK
+			],
+			[ROLES.HEALERS]: [],
+			[ROLES.TANKS]: [
+				CLASSES.WARRIOR,
+				CLASSES.DRUID
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Frost',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.DRUID
+			],
+			[ROLES.RANGED]: [
+				CLASSES.SHAMAN,
+				CLASSES.MAGE
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.SHAMAN,
+				CLASSES.DRUID,
+				CLASSES.MONK
+			],
+			[ROLES.TANKS]: []
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Holy',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.PALADIN
+			],
+			[ROLES.RANGED]: [],
+			[ROLES.HEALERS]: [
+				CLASSES.PALADIN,
+				CLASSES.PRIEST
+			],
+			[ROLES.TANKS]: [
+				CLASSES.PALADIN
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Iron',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.WARRIOR,
+				CLASSES.SHAMAN,
+				CLASSES.MONK,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.HUNTER
+			],
+			[ROLES.HEALERS]: [],
+			[ROLES.TANKS]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.PALADIN,
+				CLASSES.WARRIOR,
+				CLASSES.DEMON_HUNTER,
+				CLASSES.MONK
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Life',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DRUID
+			],
+			[ROLES.RANGED]: [
+				CLASSES.HUNTER,
+				CLASSES.DRUID
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.PALADIN,
+				CLASSES.SHAMAN,
+				CLASSES.DRUID,
+				CLASSES.MONK,
+				CLASSES.PRIEST
+			],
+			[ROLES.TANKS]: [
+				CLASSES.DRUID,
+				CLASSES.MONK
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Shadow',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.DEATH_KNIGHT,
+				CLASSES.WARRIOR,
+				CLASSES.DEMON_HUNTER,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.PRIEST,
+				CLASSES.WARLOCK
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.PRIEST
+			],
+			[ROLES.TANKS]: [
+				CLASSES.DEATH_KNIGHT
+			]
+		}
+	},
+
+	{
+		id: newId('button'),
+
+		name: 'Storm',
+		order: 10,
+
+		select: {
+			[ROLES.MELEE]: [
+				CLASSES.WARRIOR,
+				CLASSES.SHAMAN,
+				CLASSES.MONK,
+				CLASSES.ROGUE
+			],
+			[ROLES.RANGED]: [
+				CLASSES.HUNTER,
+				CLASSES.SHAMAN
+			],
+			[ROLES.HEALERS]: [
+				CLASSES.MONK
+			],
+			[ROLES.TANKS]: [
+				CLASSES.MONK
+			]
+		}
 	}
 ];
 
 window.buttons = buttons;
 window.instances = instances;
 window.items = items;
-
-// select: {
-// 	[ROLES.MELEE]: [
-
-// 	],
-// 	[ROLES.RANGED]: [
-
-// 	],
-// 	[ROLES.HEALERS]: [
-
-// 	],
-// 	[ROLES.TANKS]: [
-
-// 	]
-// }
